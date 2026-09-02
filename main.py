f8dce29502dfaed8082151a0fbce4144128da009
Başak <basaksakalli00@gmail.com>
2026-09-02 16:01:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,33 @@ def main():
     args = parser.parse_args()
 
     try:
-        user_prompt = input("Enter your project prompt: ")
+        basic_user_prompt = input(    # in this part I changed user_prompt into basic_user_prompt
+            "Enter your project prompt: "
+        )
+        design_user_prompt = input(
+            "Choose a style for your project. The styles are:\n"
+            "M - Modern: clean, structured, and minimal design\n"
+            "P - Playful: fun, dynamic, and creative design\n"
+            "E - Elegant: sophisticated, refined, and minimal design\n"
+            "G - Girly: cute, soft, and aesthetic design\n"
+            "D - Dark: bold, sleek, and high-contrast design\n"
+            "C - Classic: simple, balanced, and timeless design\n"
+            "For custom style you can directly type it\n"
+            "Your choice: "
+        )
+        color_user_prompt = input(
+            "Choose a color theme for your project.\n"
+            "B - Blue\n"
+            "P - Pink\n"
+            "G - Green\n"
+            "O - Orange\n"
+            "M - Monochrome (black, white, gray)\n"
+            "For custom color theme you can directly type it\n"
+            "Your choice: "
+        )
+
+        user_prompt = user_prompt_logic.get_user_prompt(basic_user_prompt, design_user_prompt, color_user_prompt)
+        
         result = agent.invoke(
             {"user_prompt": user_prompt},
             {"recursion_limit": args.recursion_limit}
